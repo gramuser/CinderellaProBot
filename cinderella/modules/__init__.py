d1f2e2e76cef8d9e5ad82337a94868cf62c031e9
gramuser <68482417+gramuser@users.noreply.github.com>
2020-07-31 02:01:49 +0530

    Update

--- cinderella/modules/__init__.py
+++ cinderella/modules/__init__.py
@@ -32,18 +32,3 @@ def __list_all_modules():
 ALL_MODULES = sorted(__list_all_modules())
 LOGGER.info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
-
-from userbot import catdef
-import requests
-import os
-from userbot.uniborgConfig import Config
-
-if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-     os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
-#thumb image
-with open(thumb_image_path, "wb") as f:
-    f.write(requests.get(Config.THUMB_IMAGE).content)
-
-catmusic = catdef.catmusic 
-catmusicvideo = catdef.catmusicvideo
